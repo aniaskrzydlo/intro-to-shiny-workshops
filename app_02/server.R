87407809d2c59f6f94613e5182254b56433d7d56
aniaskrzydlo <anna.skrzydlo@appsilon.com>
2022-05-17 21:00:54 +0200

    Increase font of plot label text

--- app_02/server.R
+++ app_02/server.R
@@ -5,6 +5,8 @@ server <- function(input, output, session) {
     ggplot(iris, aes_string("Species", input$variable)) +
       geom_boxplot(fill = "royalblue") +
       theme_classic() + 
+      theme(axis.text = element_text(size = 15),
+            axis.title = element_text(size = 20)) +
       ylab(gsub(".", " ", input$variable, fixed = TRUE))
   })
   
--- app_04/server.R
+++ app_04/server.R
@@ -7,6 +7,8 @@ server <- function(input, output, session) {
     ggplot(iris, aes_string("Species", input$variable)) +
       geom_boxplot(fill = "royalblue") +
       theme_classic() + 
+      theme(axis.text = element_text(size = 15),
+            axis.title = element_text(size = 20)) +
       ylab(var_title())
   })
   
